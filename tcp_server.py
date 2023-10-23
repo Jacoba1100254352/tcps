f69db8963d5d728d51a25baaf397a7f45dc90cf6
Jacob Anderson <jacobdanderson@gmail.com>
2023-10-22 23:16:38 -0600

    Final server :)

--- tcp_server.py
+++ tcp_server.py
@@ -2,7 +2,13 @@ import logging, random, socket, argparse, struct, sys
 
 
 def randomize_text(text):
-    """Randomize text by repeating characters and discarding some of them."""
+    """
+    Randomize text by repeating characters and discarding some of them.
+
+    :param text: The input text to be randomized.
+    :return: A randomized version of the input text.
+    """
+
     def discard():
         return random.choices([True, False], weights=[1, 5])[0]
 
@@ -15,22 +21,35 @@ def randomize_text(text):
 
 
 def parse_header(header):
-    """Parse the binary header to extract action and message length."""
+    """
+    Parse the binary header to extract action and message length.
+
+    :param header: The binary header to be parsed.
+    :return: A tuple containing the action and message length.
+    """
+    # Variables for decoding header
     binary_header = format(header, "032b")
     action_code = int(binary_header[:5], 2)
     message_length = int(binary_header[5:], 2)
-    actions = {
-        1: "uppercase",
-        2: "lowercase",
-        4: "reverse",
-        8: "shuffle",
-        16: "random"
-    }
+
+    # Log
+    logging.info(
+        f"Header: {header}, Binary Header: {binary_header}, actionCode: {action_code}, Message Length: {message_length}"
+    )
+
+    # Define and return actions and info
+    actions = {1: "uppercase", 2: "lowercase", 4: "reverse", 8: "shuffle", 16: "random"}
     return actions.get(action_code, "error"), message_length
 
 
 def process_data(action, data):
-    """Process the data based on the given action."""
+    """
+    Process the data based on the given action.
+
+    :param action: The action to be applied to the data.
+    :param data: The input data to be processed.
+    :return: The processed data based on the specified action.
+    """
     if action == "uppercase":
         return data.upper()
     elif action == "lowercase":
@@ -46,7 +65,12 @@ def process_data(action, data):
 
 
 def run(port):
-    """Run the server."""
+    """
+    Run the server.
+
+    :param port: The port number on which the server listens.
+    :return: None
+    """
     server_socket = socket.socket()
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(("", port))
@@ -63,7 +87,6 @@ def run(port):
                 chunk = conn.recv(1024)
                 if not chunk:
                     break
-
                 data_buffer.extend(chunk)
 
                 while len(data_buffer) >= 4:
@@ -73,32 +96,49 @@ def run(port):
                     if len(data_buffer) < 4 + msg_len:
                         break
 
-                    message = data_buffer[4:4+msg_len].decode("utf-8", "ignore")
+                    message = data_buffer[4 : 4 + msg_len].decode("utf-8", "ignore")
+                    logging.info(f"Received: {message}")
                     response = process_data(action, message)
                     conn.send(struct.pack("!I", len(response)) + response.encode())
 
-                    data_buffer = data_buffer[4+msg_len:]
+                    data_buffer = data_buffer[4 + msg_len :]
 
             conn.close()
+            logging.info(f"Connection closed")
     except KeyboardInterrupt:  # Ctrl-C
         print("Shutting down server")
         sys.exit(0)
 
 
 def setup_logging(verbose):
-    """Set up logging based on verbosity."""
+    """
+    Set up logging based on verbosity.
+
+    :param verbose: A boolean indicating whether to enable verbose logging.
+    :return: None
+    """
     level = logging.DEBUG if verbose else logging.INFO
     logging.basicConfig(format="%(levelname)s:%(message)s", level=level)
 
 
 if __name__ == "__main__":
-
+    # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", required=False, type=int, default=8083, help="port to bind to")
-    parser.add_argument("-v", "--verbose", required=False, action="store_true", help="turn on debugging output")
+    parser.add_argument(
+        "-p", "--port", required=False, type=int, default=8083, help="port to bind to"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        required=False,
+        action="store_true",
+        help="turn on debugging output",
+    )
     args = parser.parse_args()
 
+    # Log and setup logging
     setup_logging(args.verbose)
     logging.debug(args)
-    run(args.port)
 
+    # Run the code
+    run(args.port)
